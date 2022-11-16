2b3e88171361d77170fc73296d732f38960dfc16
Momo <84031783+LGDMomo@users.noreply.github.com>
2022-11-16 20:26:31 +0100

    Update ice.py

--- ice.py
+++ ice.py
@@ -14,7 +14,7 @@ def main():
     Scanning tool simplified my LGD
      """)
     
-    n = input(" 1- Scan Network Port    \n 2- Vulnerabilities Scanning      \n 3- Exploit Vulnerabilities    \n 4- Ping Adresse \n 5- Network Scanning device \n 6- Classic Scan \n 7- Hack Automatisation \n 8- Auto Hack Matteo \n 9- List All Device On Network \n Choose an option : ")
+    n = input(" 1- Scan Network Port    \n 2- Vulnerabilities Scanning      \n 3- Exploit Vulnerabilities    \n 4- Ping Adresse \n 5- Network Scanning device \n 6- Classic Scan \n 7- Hack Automatisation \n 8- List All Device On Network \n Choose an option : ")
     if n == '1':
         nmap()
     if n == '2':
@@ -31,8 +31,6 @@ def main():
     if n == '7':
         auto()
     if n == '8':
-        matteoice()
-    if n == '9':
         liste()
     else:
         print("Entrez une bonne option")
@@ -71,78 +69,6 @@ def nmap():
                         else:
                             print("No Open Ports Were Found")
                             main()
-                            
-                                   
-                            
-                            
-
-    
-        
-
-def matteoice():
-        print("Matteo Connection")
-        ip1 = ("192.168.8.1/24")
-        ip2 = ("192.168.9.1/24")
-        ip3 = ("192.168.10.1/24")
-        ip4 = ("192.168.11.1/24")
-        print("\n\n"+ip1+" First Main Router\n")
-        print(ip2+" Seconde Main Router\n")
-        print(ip3+" Third Main Router\n")
-        print(ip4+" Fourth Main Router\n")
-        print("\n\n\n")
-        print("Loading please wait . . .")
-        print("\n\n\n\n\n\n\n")
-        sc.scan(ip1,arguments=('-p 80,5900'))
-        for host in sc.all_hosts():
-            for proto in sc[host].all_protocols():
-                lport = sc[host][proto].keys()
-                for port in lport:
-                    if sc[host][proto][port]['state'] == "open":
-                        print('Host : %s %s' % (host, sc[host].hostname()))
-                        print ('port : %s\tstate : %s' % (port, sc[host][proto][port]['state']))
-                        print("---------------------------")
-                        print("\n\n\n\n\n\n\n\n\n\n\n")
-        sc.scan(ip1,arguments=('-p 80,5900'))
-        for host in sc.all_hosts():
-            for proto in sc[host].all_protocols():
-                lport = sc[host][proto].keys()
-                for port in lport:
-                    if sc[host][proto][port]['state'] == "open":
-                        print('Host : %s %s' % (host, sc[host].hostname()))
-                        print ('port : %s\tstate : %s' % (port, sc[host][proto][port]['state']))
-                        print("---------------------------")
-                        print("\n\n\n\n\n\n\n\n\n\n\n")
-        sc.scan(ip2,arguments=('-p 80,5900'))
-        for host in sc.all_hosts():
-            for proto in sc[host].all_protocols():
-                lport = sc[host][proto].keys()
-                for port in lport:
-                    if sc[host][proto][port]['state'] == "open":
-                        print('Host : %s %s' % (host, sc[host].hostname()))
-                        print ('port : %s\tstate : %s' % (port, sc[host][proto][port]['state']))
-                        print("---------------------------")
-                        print("\n\n\n\n\n\n\n\n\n\n\n")
-        sc.scan(ip3,arguments=('-p 80,5900'))
-        for host in sc.all_hosts():
-            for proto in sc[host].all_protocols():
-                lport = sc[host][proto].keys()
-                for port in lport:
-                    if sc[host][proto][port]['state'] == "open":
-                        print('Host : %s %s' % (host, sc[host].hostname()))
-                        print ('port : %s\tstate : %s' % (port, sc[host][proto][port]['state']))
-                        print("---------------------------")
-                        print("\n\n\n\n\n\n\n\n\n\n\n")
-        sc.scan(ip4,arguments=('-p 80,5900'))
-        for host in sc.all_hosts():
-            for proto in sc[host].all_protocols():
-                lport = sc[host][proto].keys()
-                for port in lport:
-                    if sc[host][proto][port]['state'] == "open":
-                        print('Host : %s %s' % (host, sc[host].hostname()))
-                        print ('port : %s\tstate : %s' % (port, sc[host][proto][port]['state']))
-                        print("---------------------------")
-                        print("\n\n\n\n\n\n\n\n\n\n\n")
-
 
 def vuln():
     print("Vulnerabilities Scanning")
@@ -229,8 +155,6 @@ def liste():
         print("Network Ip = ",ip)
         print("\n\n\n")
         os.system('nmap -sn '+ip)
-
-
 
 
 if __name__ == '__main__':
