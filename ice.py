e6e568b8551dc96f2f5e2579132f2d2f08854e56
Rainbot <84031783+AutumnRainBot@users.noreply.github.com>
2025-07-28 19:01:07 +0200

    Update ice.py

--- ice.py
+++ ice.py
@@ -6,7 +6,7 @@ sc = nmap.PortScanner()
 
 def main():
     print("""
-    Scanning tool simplified my LGD
+    Scanning tool simplified by LGD
      """)
     
     n = input(" 1- Scan Network Port    \n 2- Vulnerabilities Scanning      \n 3- Exploit Vulnerabilities    \n 4- Ping Adresse \n 5- Network Scanning device \n 6- Classic Scan \n 7- Hack Automatisation \n 8- List All Device On Network \n Choose an option : ")
